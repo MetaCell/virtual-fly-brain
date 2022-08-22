ed4baa404be9e0c7e42a4fcb64bc9acb0366330e
enicolasgomez <enicolasgomez@gmail.com>
2022-08-22 09:18:45 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { VFBMain } from './components/VFBMain'
+
+function App() {
+  return (
+    <div className="App">
+      <VFBMain></VFBMain>
+    </div>
+  );
+}
+
+export default App;

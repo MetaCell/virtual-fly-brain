ea552f3fba2136fb5afd174d8f084a2397e3af7c
enicolasgomez <enicolasgomez@gmail.com>
2022-12-19 12:58:13 -0300

    Changes to cloud harness deployment

--- applications/virtual-fly-brain/backend/pub_chem_index/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-
-# flake8: noqa
-from __future__ import absolute_import
-# import models into model package
-#from virtual_fly_brain.models.molecule import Molecule
-#from virtual_fly_brain.models.molecule_inchi import MoleculeInchi

87ab66aa3bf60176115373ed413296f2ab53aa06
jrmartin <yeshuarmartinez@gmail.com>
2023-01-24 08:45:07 -0800

    #VFB-2 Update api generated classes

---
+++ applications/virtual-fly-brain/backend/virtual_fly_brain/controllers/test_controller.py
@@ -0,0 +1,15 @@
+import connexion
+import six
+
+from virtual_fly_brain import util
+
+
+def test_api():  # noqa: E501
+    """Test API
+
+    Test API. # noqa: E501
+
+
+    :rtype: str
+    """
+    return 'do some magic!'

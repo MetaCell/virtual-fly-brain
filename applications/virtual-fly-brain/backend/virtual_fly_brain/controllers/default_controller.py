edbc7fce28353bbe4e120adb4c3ec5f00e43c571
jrmartin <yeshuarmartinez@gmail.com>
2023-03-27 17:20:32 -0700

    #vfb-12 api fix

--- applications/virtual-fly-brain/backend/virtual_fly_brain/controllers/default_controller.py
+++ applications/virtual-fly-brain/backend/virtual_fly_brain/controllers/default_controller.py
@@ -6,7 +6,6 @@ from flask import request
 def test_api():  # noqa: E501
     return "Test Endpoint"
 
-def get_stack_viewer_images():
-    id = request.args.get('id')
+def get_stack_viewer_images(id):
     stack_viewer_data = vfb.get_instances(id)
     return stack_viewer_data

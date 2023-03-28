911c2ee27b7d7a88aba1cf2d299828105dcb7ab1
jrmartin <yeshuarmartinez@gmail.com>
2023-03-28 13:45:35 -0700

    #vfb-12 remove duplicate functions

--- applications/virtual-fly-brain/backend/virtual_fly_brain/__main__.py
+++ applications/virtual-fly-brain/backend/virtual_fly_brain/__main__.py
@@ -8,29 +8,10 @@ import os
 import json
 
 def init_webapp_routes(app):
-    www_path = os.path.dirname(os.path.abspath(__file__)) + "/www"
-
     @app.route('/get_instances', methods=['GET'])
     @cross_origin(supports_credentials=True)
     def instances():
       return vfb.get_instances(request.args.get('short_form'))
-
-    # @app.route('/', methods=['GET'])
-    # def index():
-    #     return flask.send_from_directory(www_path, 'index.html')
-
-    @app.route('/<path:path>', methods=['GET'])
-    def send_webapp(path):
-        return flask.send_from_directory(www_path, path)
-
-    @app.errorhandler(404)
-    def page_not_found(error):
-        # when a 404 is thrown send the "main" index page
-        # unless the first segment of the path is in the exception list
-        first_segment_path = flask.request.full_path.split('/')[1]
-        if first_segment_path in ['api', 'static', 'test']:  # exception list
-            return error
-        return index()
 
 app = init_flask(title="VFB index API", webapp=True, init_app_fn=init_webapp_routes)
 

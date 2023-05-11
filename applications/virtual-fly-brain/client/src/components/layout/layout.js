5fb17d9062f2793b895e389e25a2fb204327b060
enicolasgomez <enicolasgomez@gmail.com>
2023-05-11 15:07:22 -0300

    Basic layout implementation

---
+++ applications/virtual-fly-brain/client/src/components/layout/layout.js
@@ -0,0 +1,68 @@
+/**
+ * The default layout of the application.
+ * 
+ * Will be loaded by FlexLayout.
+ * https://github.com/caplin/FlexLayout
+ */
+export const layout = {
+  global: {
+    tabEnableClose: true,
+    tabSetHeaderHeight: 26,
+    tabSetTabStripHeight: 26,
+    enableEdgeDock: false,
+  },
+  borders: [
+    {
+      type: 'border',
+      location: 'bottom',
+      size: 100,
+      children: [],
+      barSize: 35,
+    },
+  ],
+  "layout": {
+    "type": "tabset",
+    "weight": 100,
+    "id": "root",
+    "children": [
+      {
+        "type": "row",
+        "weight": 69,
+        "id": "headerBar",
+        "children": []
+      },
+      {
+        "type": "row",
+        "weight": 69,
+        "children": [
+          {
+            "type": "tabset",
+            "weight": 100,
+            "id": "mainPanel",
+            "enableDeleteWhenEmpty": false,
+            "children": [
+              {
+                type: "tab",
+                name: "left",
+                id: "leftPanel",
+                component: "panel",
+              }
+              ,{
+                type: "tab",
+                name: "center",
+                id: "centerPanel", 
+                component: "panel",
+              }
+              ,{
+                type: "tab",
+                name: "right",
+                id: "rightPanel", 
+                component: "panel",
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+};
--- applications/virtual-fly-brain/client/src/store.js
+++ applications/virtual-fly-brain/client/src/store.js
@@ -1,15 +1,26 @@
-import { legacy_createStore as createStore, combineReducers } from 'redux'
+import { combineReducers } from 'redux';
+import { createStore } from '@metacell/geppetto-meta-client/common';
 import './index.css';
 import TermInfoReducer from './reducers/TermInfoReducer';
 import ThreeDCanvasReducer from './reducers/ThreeDCanvasReducer';
 import GlobalReducer from './reducers/GlobalReducer';
+import { layout as baseLayout } from './components/layout/layout'; 
+import componentMap from './components/layout/componentMap'; 
 
-const rootReducer = combineReducers({
+const INIT_STATE = {
+};
+
+const reducers = combineReducers({
   termInfo: TermInfoReducer,
   threeD: ThreeDCanvasReducer,
   globalInfo : GlobalReducer
 });
 
-const store = createStore(rootReducer);
+const store = createStore(
+  reducers,
+  INIT_STATE,
+  null,
+  { baseLayout, componentMap }
+)
 
 export default store; 

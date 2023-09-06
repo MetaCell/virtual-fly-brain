e6ab0e22b0761e6d7b1a556525609d80bd25c9c1
vidhya-metacell <vidhya@metacell.us>
2023-09-06 15:57:30 +0200

    #VFB-51 Subheader updates

--- applications/virtual-fly-brain/client/src/shared/subHeader/SearchBuilder.js
+++ applications/virtual-fly-brain/client/src/shared/subHeader/SearchBuilder.js
@@ -45,6 +45,7 @@ const InputWrapper = styled('div')(
     min-width: 1.875rem;
     flex-grow: 1;
     border: 0;
+    cursor: pointer;
     margin: 0;
     outline: 0;
     text-overflow: ellipsis;

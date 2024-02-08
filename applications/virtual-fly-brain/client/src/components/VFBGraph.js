3748211665c7daec18e66c4d024d5f17cca98f7a
enicolasgomez <enicolasgomez@gmail.com>
2024-02-07 21:39:14 -0300

    Implemented resize observer

--- applications/virtual-fly-brain/client/src/components/VFBGraph.js
+++ applications/virtual-fly-brain/client/src/components/VFBGraph.js
@@ -33,6 +33,7 @@ class VFBGraph extends Component {
 
   constructor (props) {
     super(props);
+    this.containerRef = React.createRef();
     this.state = {
       graph : { nodes : [], links : [] },
       currentQuery : this.props.instance != null ? this.props.instance : { id : "" , name : "" },
@@ -104,6 +105,17 @@ class VFBGraph extends Component {
       this.queryResults(cypherQuery(instanceId), { id : instanceId, name : instanceName });
     }
 
+    if (!this.resizeObserver && this.containerRef.current)
+    {
+      this.resizeObserver = new ResizeObserver(entries => {
+        this.graphRef.current.ggv.current.zoomToFit();
+      });
+  
+      if (this.containerRef.current) {
+        this.resizeObserver.observe(this.containerRef.current); // Start observing for size changes
+      }
+    }
+
     this.updateCameraAtStart();
   }
 
@@ -119,6 +131,9 @@ class VFBGraph extends Component {
   }
   componentWillUnmount () {
     this.__isMounted = false;
+    if (this.resizeObserver) {
+      this.resizeObserver.disconnect(); // Stop observing for size changes
+    }
   }
 
   resetCamera () {
@@ -429,185 +444,188 @@ class VFBGraph extends Component {
             </div>
             <p style={{ float : "right", width : "80%", paddingTop : "2vh" }}>No graph available for {this.getErrorLabel()}</p>
           </div>
-          : <Box sx={{
-            width: 600,
-            height: 800,
-            backgroundColor: 'primary.dark',
-            '&:hover': {
-              backgroundColor: 'primary.main',
-              opacity: [0.9, 0.8, 0.7],
-            },
-          }}><GeppettoGraphVisualization
-            id= { COMPONENT_ID }
-            // Graph data with Nodes and Links to populate
-            data={this.state.graph}
-            // Create the Graph as 2 Dimensional
-            d2={true}
-            onEngineStop={this.resetCamera}
-            cooldownTicks={10}
-            // Node label, used in tooltip when hovering over Node
-            nodeLabel={node => node.path}
-            nodeRelSize={20}
-            nodeSize={30}
-            // Relationship label, placed in Link
-            linkLabel={link => link.name}
-            // Assign background color to Canvas
-            backgroundColor = {stylingConfiguration.canvasColor}
-            // Assign color to Links connecting Nodes
-            linkColor = {link => {
-              let color = stylingConfiguration.linkColor;
-              if ( self.highlightLinks.has(link) ) {
-                color = self.highlightNodes.has(link.source) || self.highlightNodes.has(link.targetNode) ? stylingConfiguration.linkHoverColor : stylingConfiguration.linkColor;
-              }
+          : 
+            <div ref={this.containerRef}>
+              <Box sx={{
+              width: 600,
+              height: 800,
+              backgroundColor: 'primary.dark',
+              '&:hover': {
+                backgroundColor: 'primary.main',
+                opacity: [0.9, 0.8, 0.7],
+              },
+            }}><GeppettoGraphVisualization
+              id= { COMPONENT_ID }
+              // Graph data with Nodes and Links to populate
+              data={this.state.graph}
+              // Create the Graph as 2 Dimensional
+              d2={true}
+              onEngineStop={this.resetCamera}
+              cooldownTicks={10}
+              // Node label, used in tooltip when hovering over Node
+              nodeLabel={node => node.path}
+              nodeRelSize={20}
+              nodeSize={30}
+              // Relationship label, placed in Link
+              linkLabel={link => link.name}
+              // Assign background color to Canvas
+              backgroundColor = {stylingConfiguration.canvasColor}
+              // Assign color to Links connecting Nodes
+              linkColor = {link => {
+                let color = stylingConfiguration.linkColor;
+                if ( self.highlightLinks.has(link) ) {
+                  color = self.highlightNodes.has(link.source) || self.highlightNodes.has(link.targetNode) ? stylingConfiguration.linkHoverColor : stylingConfiguration.linkColor;
+                }
 
-              return color;
-            }}
-            nodeCanvasObject={(node, ctx, globalScale) => {
-              let cardWidth = NODE_WIDTH;
-              let cardHeight = NODE_HEIGHT;
-              let borderThickness = self.highlightNodes.has(node) ? NODE_BORDER_THICKNESS : 1;
+                return color;
+              }}
+              nodeCanvasObject={(node, ctx, globalScale) => {
+                let cardWidth = NODE_WIDTH;
+                let cardHeight = NODE_HEIGHT;
+                let borderThickness = self.highlightNodes.has(node) ? NODE_BORDER_THICKNESS : 1;
 
-              // Node border color
-              ctx.fillStyle = self.hoverNode == node ? stylingConfiguration.nodeHoverBoderColor : (self.highlightNodes.has(node) ? stylingConfiguration.neighborNodesHoverColor : stylingConfiguration.nodeBorderColor) ;
-              // Create Border
-              ctx.fillRect(node.x - cardWidth / 2 - (borderThickness), node.y - cardHeight / 2 - (borderThickness), cardWidth , cardHeight );
+                // Node border color
+                ctx.fillStyle = self.hoverNode == node ? stylingConfiguration.nodeHoverBoderColor : (self.highlightNodes.has(node) ? stylingConfiguration.neighborNodesHoverColor : stylingConfiguration.nodeBorderColor) ;
+                // Create Border
+                ctx.fillRect(node.x - cardWidth / 2 - (borderThickness), node.y - cardHeight / 2 - (borderThickness), cardWidth , cardHeight );
 
-              // Assign color to Description Area background in Node
-              ctx.fillStyle = stylingConfiguration.nodeDescriptionBackgroundColor;
-              // Create Description Area in Node
-              ctx.fillRect(node.x - cardWidth / 2,node.y - cardHeight / 2, cardWidth - (borderThickness * 2 ), cardHeight - ( borderThickness * 2) );
-              // Assign color to Title Bar background in Node
-              ctx.fillStyle = stylingConfiguration.nodeTitleBackgroundColor;
-              // Create Title Bar in Node
-              ctx.fillRect(node.x - cardWidth / 2 ,node.y - cardHeight / 2, cardWidth - ( borderThickness * 2 ), cardHeight / 3);
+                // Assign color to Description Area background in Node
+                ctx.fillStyle = stylingConfiguration.nodeDescriptionBackgroundColor;
+                // Create Description Area in Node
+                ctx.fillRect(node.x - cardWidth / 2,node.y - cardHeight / 2, cardWidth - (borderThickness * 2 ), cardHeight - ( borderThickness * 2) );
+                // Assign color to Title Bar background in Node
+                ctx.fillStyle = stylingConfiguration.nodeTitleBackgroundColor;
+                // Create Title Bar in Node
+                ctx.fillRect(node.x - cardWidth / 2 ,node.y - cardHeight / 2, cardWidth - ( borderThickness * 2 ), cardHeight / 3);
 
-              // Assign font to text in Node
-              ctx.font = stylingConfiguration.nodeFont;
-              // Assign color to text in Node
-              ctx.fillStyle = stylingConfiguration.nodeFontColor;
-              // Text in font to be centered
-              ctx.textAlign = "center";
-              ctx.textBaseline = 'middle';
-              // Create Title in Node
-              ctx.fillText(node.title, node.x, node.y - 15);
-              // Add Description text to Node
-              this.wrapText(ctx, node.path, node.x, node.y, cardWidth - (borderThickness * 2) , 5);
-            }}
-            // Overwrite Node Canvas Object
-            nodeCanvasObjectMode={node => 'replace'}
-            // bu = Bottom Up, creates Graph with root at bottom
-            dagMode="bu"
-            dagLevelDistance = {100}
-            // Handles clicking event on an individual node
-            onNodeClick = { (node,event) => this.handleNodeLeftClick(node,event) }
-            // Handles clicking event on an individual node
-            onNodeRightClick = { (node,event) => this.handleNodeRightClick(node,event) }
-            ref={this.graphRef}
-            // Disable dragging of nodes
-            enableNodeDrag={false}
-            // Allow camera pan and zoom with mouse
-            enableZoomPanInteraction={true}
-            // Width of links
-            linkWidth={1.25}
-            controls = {
-              <div style={ { position: "absolute", width: "5vh", height: "100px",zIndex: "2", color : "white" } }>
-                <Tooltip placement="right" title="Reset View">
-                  <div
-                    style={
-                      {
-                        zIndex : "2",
-                        cursor : "pointer",
-                        top : "10px",
-                        left : "10px",
-                        width: "1.3rem",
-                        height: "1.3rem",
-                        backgroundPosition: 'center',
-                        backgroundImage: `url(${stylingConfiguration.icons.home})`
+                // Assign font to text in Node
+                ctx.font = stylingConfiguration.nodeFont;
+                // Assign color to text in Node
+                ctx.fillStyle = stylingConfiguration.nodeFontColor;
+                // Text in font to be centered
+                ctx.textAlign = "center";
+                ctx.textBaseline = 'middle';
+                // Create Title in Node
+                ctx.fillText(node.title, node.x, node.y - 15);
+                // Add Description text to Node
+                this.wrapText(ctx, node.path, node.x, node.y, cardWidth - (borderThickness * 2) , 5);
+              }}
+              // Overwrite Node Canvas Object
+              nodeCanvasObjectMode={node => 'replace'}
+              // bu = Bottom Up, creates Graph with root at bottom
+              dagMode="bu"
+              dagLevelDistance = {100}
+              // Handles clicking event on an individual node
+              onNodeClick = { (node,event) => this.handleNodeLeftClick(node,event) }
+              // Handles clicking event on an individual node
+              onNodeRightClick = { (node,event) => this.handleNodeRightClick(node,event) }
+              ref={this.graphRef}
+              // Disable dragging of nodes
+              enableNodeDrag={false}
+              // Allow camera pan and zoom with mouse
+              enableZoomPanInteraction={true}
+              // Width of links
+              linkWidth={1.25}
+              controls = {
+                <div style={ { position: "absolute", width: "5vh", height: "100px",zIndex: "2", color : "white" } }>
+                  <Tooltip placement="right" title="Reset View">
+                    <div
+                      style={
+                        {
+                          zIndex : "2",
+                          cursor : "pointer",
+                          top : "10px",
+                          left : "10px",
+                          width: "1.3rem",
+                          height: "1.3rem",
+                          backgroundPosition: 'center',
+                          backgroundImage: `url(${stylingConfiguration.icons.home})`
+                        }
                       }
-                    }
-                    onClick={this.resetCamera }>
-                  </div>
-                </Tooltip>
-                <Tooltip placement="right" title="Zoom In">
-                  <div
-                    style={
-                      {
-                        zIndex : "2",
-                        cursor : "pointer",
-                        marginTop : "20px",
-                        left : "10px",
-                        width: "1.3rem",
-                        height: "1.3rem",
-                        backgroundPosition: 'center',
-                        backgroundImage: `url(${stylingConfiguration.icons.zoomIn})`
+                      onClick={this.resetCamera }>
+                    </div>
+                  </Tooltip>
+                  <Tooltip placement="right" title="Zoom In">
+                    <div
+                      style={
+                        {
+                          zIndex : "2",
+                          cursor : "pointer",
+                          marginTop : "20px",
+                          left : "10px",
+                          width: "1.3rem",
+                          height: "1.3rem",
+                          backgroundPosition: 'center',
+                          backgroundImage: `url(${stylingConfiguration.icons.zoomIn})`
+                        }
                       }
-                    }
-                    onClick={this.zoomIn }>
-                  </div>
-                </Tooltip>
-                <Tooltip placement="right" title="Zoom Out">
-                  <div
-                    style={
-                      {
-                        zIndex : "2",
-                        cursor : "pointer",
-                        marginTop : "5px",
-                        left : "10px",
-                        width: "1.3rem",
-                        height: "1.3rem",
-                        backgroundPosition: 'center',
-                        backgroundImage: `url(${stylingConfiguration.icons.zoomOut})`
+                      onClick={this.zoomIn }>
+                    </div>
+                  </Tooltip>
+                  <Tooltip placement="right" title="Zoom Out">
+                    <div
+                      style={
+                        {
+                          zIndex : "2",
+                          cursor : "pointer",
+                          marginTop : "5px",
+                          left : "10px",
+                          width: "1.3rem",
+                          height: "1.3rem",
+                          backgroundPosition: 'center',
+                          backgroundImage: `url(${stylingConfiguration.icons.zoomOut})`
+                        }
                       }
-                    }
-                    onClick={this.zoomOut }>
-                  </div>
-                </Tooltip>
-                <DropDownQueries
-                  handleMenuClick={selection => self.handleMenuClick(selection)}
-                  currentQuery = {self.state.currentQuery}
-                  focusedInstance = {self.focusedInstance}
-                  sync = { () => self.sync() }
-                  syncColor = { syncColor }
-                  stylingConfiguration = { stylingConfiguration }
-                />
-              </div>
-            }
-            click={() => self.graphRef.current.ggv.current.zoomToFit()}
-            // Function triggered when hovering over a nodeoptions
-            onNodeHover={node => {
-              // Reset maps of hover nodes and links
-              self.highlightNodes.clear();
-              self.highlightLinks.clear();
-
-              // We found the node that we are hovering over
-              if (node) {
-                // Keep track of hover node, its neighbors and links
-                self.highlightNodes.add(node);
-                node.neighbors.forEach(neighbor => self.highlightNodes.add(neighbor));
-                node.links.forEach(link => self.highlightLinks.add(link));
+                      onClick={this.zoomOut }>
+                    </div>
+                  </Tooltip>
+                  <DropDownQueries
+                    handleMenuClick={selection => self.handleMenuClick(selection)}
+                    currentQuery = {self.state.currentQuery}
+                    focusedInstance = {self.focusedInstance}
+                    sync = { () => self.sync() }
+                    syncColor = { syncColor }
+                    stylingConfiguration = { stylingConfiguration }
+                  />
+                </div>
               }
+              click={() => self.graphRef.current.ggv.current.zoomToFit()}
+              // Function triggered when hovering over a nodeoptions
+              onNodeHover={node => {
+                // Reset maps of hover nodes and links
+                self.highlightNodes.clear();
+                self.highlightLinks.clear();
 
-              // Keep track of hover node
-              self.hoverNode = node || null;
-              document.getElementById(COMPONENT_ID).style.cursor = node ? '-webkit-grab' : null;
-            }
-            }
-            // Function triggered when hovering over a link
-            onLinkHover={link => {
-              // Reset maps of hover nodes and links
-              self.highlightNodes.clear();
-              self.highlightLinks.clear();
+                // We found the node that we are hovering over
+                if (node) {
+                  // Keep track of hover node, its neighbors and links
+                  self.highlightNodes.add(node);
+                  node.neighbors.forEach(neighbor => self.highlightNodes.add(neighbor));
+                  node.links.forEach(link => self.highlightLinks.add(link));
+                }
 
-              // We found link being hovered
-              if (link) {
-                // Keep track of hovered link, and it's source/target node
-                self.highlightLinks.add(link);
-                self.highlightNodes.add(link.source);
-                self.highlightNodes.add(link.target);
+                // Keep track of hover node
+                self.hoverNode = node || null;
+                document.getElementById(COMPONENT_ID).style.cursor = node ? '-webkit-grab' : null;
               }
-            }
-            }
-          /></Box>
+              }
+              // Function triggered when hovering over a link
+              onLinkHover={link => {
+                // Reset maps of hover nodes and links
+                self.highlightNodes.clear();
+                self.highlightLinks.clear();
+
+                // We found link being hovered
+                if (link) {
+                  // Keep track of hovered link, and it's source/target node
+                  self.highlightLinks.add(link);
+                  self.highlightNodes.add(link.source);
+                  self.highlightNodes.add(link.target);
+                }
+              }
+              }
+            /></Box>
+          </div>
     )
   }
 }

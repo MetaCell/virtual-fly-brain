416656115ad142431ea5aa5415dad7d9fc105222
enicolasgomez <enicolasgomez@gmail.com>
2024-03-06 16:40:49 -0300

    Changes to updaterMiddleware

--- applications/virtual-fly-brain/client/src/urlUpdaterMiddleware.js
+++ applications/virtual-fly-brain/client/src/urlUpdaterMiddleware.js
@@ -1,38 +1,99 @@
 import { getInstancesTypes } from './reducers/actions/types/getInstancesTypes';
 import { getQueriesTypes } from './reducers/actions/types/getQueriesTypes';
+import { getInstanceByID } from './reducers/actions/instances';
+import { get3DMesh } from './reducers/actions/instances';
+import { get_instance } from './network/query';
 
 function updateUrlParameterWithCurrentUrl(param, value) {
   const urlObj = new URL(window.location.href);
   urlObj.searchParams.set(param, value);
-  window.history.replaceState(window.history.state, '', urlObj.toString());
+  window.history.replaceState(null, '', urlObj.toString());
 }
+async function fetchAndDispatchMeshes(threeDMeshes) {
+  for (const id of threeDMeshes) {
+    const instance = await get_instance(id);
+    get3DMesh(instance);
+  }
+}
+
+const DEFAULT_ID = "VFB_00101567"; 
+const APP_LOADED_FLAG_KEY = "CURRENT_LOADED_URL";
+
+const isFirstTimeLoad = () => {
+  const appLoadedUrl = localStorage.getItem(APP_LOADED_FLAG_KEY);
+  const currentUrl = window.location.href ;
+  if ( currentUrl != appLoadedUrl )
+  {
+    localStorage.setItem(APP_LOADED_FLAG_KEY, currentUrl);
+    return true ;
+  }
+  return false ;
+};
+
+const getUrlParameter = (param) => {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(param);
+};
 
 export const urlUpdaterMiddleware = store => next => action => {
   next(action);
 
-// id= specify the focus term we should have when the ui loads
-// id= supports only 1 id
-// if the user gives us an array of ids (comma separated) just parse the first
-// if the first does not exist just go to the template id and set this one as the focus term
-// the default id that we load is VFB_00101567, so if we try to load something and we don’t get anything in return just load the default template.
-// this paramater circle back to the url
+  if (isFirstTimeLoad()) {
+    // Load id parameter from URL and dispatch action
+    const idFromUrl = getUrlParameter("id");
+    let idToUpdate = undefined;
 
-  if (action.type === getInstancesTypes.GET_INSTANCES_SUCCESS) {
-    updateUrlParameterWithCurrentUrl("id", action.payload.Id);
+    if (idFromUrl) {
+      if (typeof idFromUrl === 'string' && idFromUrl.includes(',')) {
+        const firstIdToUpdate = idFromUrl.split(',')[0];
+        idToUpdate = firstIdToUpdate;
+      } else {
+        idToUpdate = idFromUrl;
+      }
+    }
+    if (!idToUpdate) {
+      idToUpdate = DEFAULT_ID;
+    }
+
+    getInstanceByID(idToUpdate);
+
+    // Load i parameter from URL and dispatch action
+    const iFromUrl = getUrlParameter("i");
+    let threeDMeshes = [];
+
+    if (iFromUrl) {
+      if (typeof iFromUrl === 'string' && iFromUrl.includes(',')) {
+        threeDMeshes = iFromUrl.split(',');
+      } else {
+        threeDMeshes.push(iFromUrl);
+      }
+    }
+
+    if ((threeDMeshes.length == 0 && idToUpdate) || (threeDMeshes.length > 0 && threeDMeshes.indexOf(idToUpdate) == -1))
+      threeDMeshes.push(idToUpdate);
+    
+    fetchAndDispatchMeshes(threeDMeshes);
+
+    // Load q parameter from URL and dispatch action
+    // const qFromUrl = getUrlParameter("q");
+    // if (qFromUrl) {
+
+    // }
   }
 
-  // i= specify the list of ids that we should have in the scene when the ui is loaded
-  // this could be an array of ids
-  // all the ids that are passed to this parameter are part of the scene upon loding the url.
-  // this paramater circle back to the url
+  if (action.type === getInstancesTypes.GET_INSTANCES_SUCCESS) {
+    let idToUpdate = action.payload.Id;
+    updateUrlParameterWithCurrentUrl("id", idToUpdate);
+  }
 
   if (action.type === getInstancesTypes.GET_3D_OBJ_TYPE_SUCCESS) {
     updateUrlParameterWithCurrentUrl("i", action.payload.id);
   }
 
-  // q= specify which query results we want to have opened by default for a certain id
-  // when using q= the query results component should be opened by default, even if the results to display are 0.
-  // q= needs to support the query type that we use in the vfbqueries library (e.g. ListAllAvailableImages, SimilarMorphologyTo, etc), and comma separated the id instance for which
+  if (action.type === getQueriesTypes.UPDATE_QUERIES || action.type === getQueriesTypes.GET_QUERIES_SUCCESS) {
+    updateUrlParameterWithCurrentUrl("q", action.payload.shortform);
+  }
+
   if (action.type === getQueriesTypes.UPDATE_QUERIES || action.type === getQueriesTypes.GET_QUERIES_SUCCESS) {
     updateUrlParameterWithCurrentUrl("q", action.payload.shortform);
   }

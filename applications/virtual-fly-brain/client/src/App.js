3f059039b74fe664568bd3a19dc678247b411cf5
enicolasgomez <enicolasgomez@gmail.com>
2024-03-07 10:56:49 -0300

    Changed initial load mechanism

--- applications/virtual-fly-brain/client/src/App.js
+++ applications/virtual-fly-brain/client/src/App.js
@@ -2,25 +2,7 @@
 import Main from './components/Main'
 import React, { useEffect, useState } from 'react';
 
-function clearAppStorage() {
-  Object.keys(localStorage).forEach(key => {
-    localStorage.removeItem(key);
-  });
-  Object.keys(sessionStorage).forEach(key => {
-    sessionStorage.removeItem(key);
-  });
-}
-
 const App = () => {
-  const [loadedOnce, setLoadedOnce] = useState(false);
-
-  useEffect(() => {
-    if (!loadedOnce) {
-      setLoadedOnce(true);
-      clearAppStorage();
-    }
-  }, []); 
-
   return (
     <Main />
   );

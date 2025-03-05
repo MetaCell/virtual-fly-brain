f3ba2a60e27e075ed17a54f980d537684050ce8f
Rob Court <robbie.court@gmail.com>
2025-03-05 19:27:34 +0000

    Revert "update Dockerfile to configure yarn and install dependencies with legacy peer deps"
    This reverts commit c56c3f9e9ab5feb4dd59aaa317205107f4e5f45b.

--- applications/virtual-fly-brain/client/webpack.config.js
+++ applications/virtual-fly-brain/client/webpack.config.js
@@ -16,10 +16,9 @@ module.exports = function webpacking(envVariables) {
     env.mode = 'production';
   }
 
-  // Use environment variables for API URLs if available
   const API_URL = {
-    production: JSON.stringify(process.env.API_URL_PRODUCTION || 'https://vfb.dev.metacell.us/'),
-    development: JSON.stringify(process.env.API_URL_DEVELOPMENT || 'http://localhost:8080/')
+    production: JSON.stringify('https://vfb.dev.metacell.us/'),
+    development: JSON.stringify('http://localhost:8080/')
   }
 
   // check environment mode
@@ -102,9 +101,7 @@ module.exports = function webpacking(envVariables) {
 
   const plugins = [
     new webpack.DefinePlugin({
-      'API_URL': API_URL[environment],
-      'process.env.API_URL_PRODUCTION': JSON.stringify(process.env.API_URL_PRODUCTION),
-      'process.env.API_URL_DEVELOPMENT': JSON.stringify(process.env.API_URL_DEVELOPMENT)
+      'API_URL': API_URL[environment]
     }),
     new CleanWebpackPlugin(),
     new CopyWebpackPlugin({ patterns: copyPaths }),

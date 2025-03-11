667a4c82f72c949ab51d1b460352680013813c51
Rob Court <robbie.court@gmail.com>
2025-03-11 20:40:19 +0000

    refactor constants for improved color transparency in selected states

--- applications/virtual-fly-brain/client/src/utils/constants.js
+++ applications/virtual-fly-brain/client/src/utils/constants.js
@@ -1,6 +1,6 @@
 export const SELECTED_COLOR = { r: 0.8, g: 0.8, b: 0, a: 1 }
-export const DESELECTED_COLOR = { r: .2, g: .8, b: 1, a: 1 }
-export const TEMPLATE_COLOR = { r: .8, g: .8, b: .8, a: 1 }
+export const DESELECTED_COLOR = { r: .2, g: .8, b: 1, a: .3 }
+export const TEMPLATE_COLOR = { r: .8, g: .8, b: .8, a: .4 }
 export const SKELETON = "skeleton";
 export const CYLINDERS = "sphere";
 export const NEURON = "Neuron"

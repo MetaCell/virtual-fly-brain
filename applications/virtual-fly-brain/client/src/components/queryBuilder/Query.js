b9f8545515005abb00357dc1007b33703b5984dd
ddelpiano <dario@metacell.us>
2025-07-09 13:42:01 +0200

    filename changed

--- applications/virtual-fly-brain/client/src/components/queryBuilder/Query.js
+++ applications/virtual-fly-brain/client/src/components/queryBuilder/Query.js
@@ -228,7 +228,7 @@ const Query = forwardRef(({ fullWidth, queries, searchTerm }, ref) => {
       const url = URL.createObjectURL(blob);
       const a = document.createElement('a');
       a.href = url;
-      a.download = 'query_results.csv';
+      a.download = `vfb_${Date.now()}.csv`;
       document.body.appendChild(a);
       a.click();
       document.body.removeChild(a);

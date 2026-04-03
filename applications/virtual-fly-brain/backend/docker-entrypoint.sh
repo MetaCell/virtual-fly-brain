5b349262f1ae8cf623b786c87838954229e4d2b5
jrmartin <jesus@metacell.us>
2026-04-03 08:55:28 -0700

    #VFB-236 implement conditional backend launch in Docker entrypoint script

--- applications/virtual-fly-brain/backend/docker-entrypoint.sh
+++ applications/virtual-fly-brain/backend/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Only start cron if backend is enabled
+# Start cron service in the background (only if backend is enabled)
 if [ "$BUILD_BACKEND" != "false" ]; then
     service cron start
 fi
@@ -9,4 +9,9 @@ fi
 mkdir -p /tmp/vfb_cache/term_info /tmp/vfb_cache/queries
 chmod 755 /tmp/vfb_cache /tmp/vfb_cache/term_info /tmp/vfb_cache/queries
 
-exec "$@"
+# Conditional launch: if backend is disabled, serve frontend only
+if [ "$BUILD_BACKEND" == "false" ]; then
+    exec serve /usr/src/app/virtual_fly_brain/www --single --listen 8080
+else
+    exec gunicorn --log-level=info --preload --bind=0.0.0.0:8080 --timeout=240 --graceful-timeout=30 --keep-alive=5 virtual_fly_brain.__main__:app
+fi

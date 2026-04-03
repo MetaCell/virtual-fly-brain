487b77d1cc82a3b15748d86ddf99e8b332c90dce
jrmartin <jesus@metacell.us>
2026-04-03 13:08:32 -0700

    #VFB-236 - add exec command to start main application after conditional backend launch in Docker entrypoint script

--- applications/virtual-fly-brain/backend/docker-entrypoint.sh
+++ applications/virtual-fly-brain/backend/docker-entrypoint.sh
@@ -13,11 +13,12 @@ fi
 mkdir -p /tmp/vfb_cache/term_info /tmp/vfb_cache/queries
 chmod 755 /tmp/vfb_cache /tmp/vfb_cache/term_info /tmp/vfb_cache/queries
 
-# Start the main application
-exec "$@"
 # Conditional launch: if backend is disabled, serve frontend only
 if [ "$BUILD_BACKEND" == "true" ]; then
     exec gunicorn --log-level=info --preload --bind=0.0.0.0:8080 --timeout=240 --graceful-timeout=30 --keep-alive=5 virtual_fly_brain.__main__:app
 else
     exec serve /usr/src/app/virtual_fly_brain/www --single --listen 8080
 fi
+
+# Start the main application
+exec "$@"

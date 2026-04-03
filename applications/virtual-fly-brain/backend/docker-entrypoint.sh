b347a2979ed827835dd1b82c2a0a9d953f474486
jrmartin <jesus@metacell.us>
2026-04-03 07:53:48 -0700

    #VFB-236 : implement conditional cron job setup in Dockerfile based on backend build option

--- applications/virtual-fly-brain/backend/docker-entrypoint.sh
+++ applications/virtual-fly-brain/backend/docker-entrypoint.sh
@@ -9,9 +9,4 @@ fi
 mkdir -p /tmp/vfb_cache/term_info /tmp/vfb_cache/queries
 chmod 755 /tmp/vfb_cache /tmp/vfb_cache/term_info /tmp/vfb_cache/queries
 
-# Conditional launch: if backend is disabled, serve frontend only
-if [ "$BUILD_BACKEND" == "false" ]; then
-    exec serve /usr/src/app/virtual_fly_brain/www --single --listen 8080
-else
-    exec "$@"
-fi
+exec "$@"

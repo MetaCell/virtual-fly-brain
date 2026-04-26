655bafc3e0caffd21cf0f77ebed00cac6755e1ae
jrmartin <jesus@metacell.us>
2026-04-26 13:45:58 -0700

    #VFB-240 - use protocol

--- applications/virtual-fly-brain/frontend/src/utils/neuroglassStateConfig.js
+++ applications/virtual-fly-brain/frontend/src/utils/neuroglassStateConfig.js
@@ -77,7 +77,7 @@ async function buildNeuroglassLayerUrl(protocol, baseUrl, instanceId) {
           );
 
           if (neuroglancerFolder) {
-            return `${vfbFolderUrl}/${neuroglancerFolder}:`;
+            return `${vfbFolderUrl}/${neuroglancerFolder}|${protocol}:`;
           }
         }
       } else {

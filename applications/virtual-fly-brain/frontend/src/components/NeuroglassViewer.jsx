f23c6ae373a213410bc231d379f290a4e6fc3ec0
jrmartin <jesus@metacell.us>
2026-04-27 10:44:24 -0700

    #vfb-240 - remove unused useMemo ref

--- applications/virtual-fly-brain/frontend/src/components/NeuroglassViewer.jsx
+++ applications/virtual-fly-brain/frontend/src/components/NeuroglassViewer.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Box, Typography, useMediaQuery } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
